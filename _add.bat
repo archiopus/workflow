1a2b0b86e7eec9b53fdf194573ab3c65926f942e
Denis Seregin <archiopus@gmail.com>
2021-09-18 00:09:30 +0300

    20210918000930

---
+++ _add.bat
@@ -0,0 +1,15 @@
+echo off
+rem chcp 1251 
+set ts=%DATE:~6,4%%DATE:~3,2%%DATE:~0,2%%TIME:~0,2%%TIME:~3,2%%TIME:~6,2%
+set ts=%ts: =0%
+echo on
+
+winrar a -m5 -r test05_"%ts%" test05
+
+git add .\*.*
+git commit -m "%ts%"
+
+rem git add .\*.txt
+rem git status
+rem git add .\*.cdw .\*.frw .\*.kdw .\*.spw .\*.a3d .\*.t3d .\*.m3d .\*.cdt .\*.frt .\*.kdt .\*.spt .\*.a3t .\*.m3t .\*.dwg .\*.dxf
+rem git status
